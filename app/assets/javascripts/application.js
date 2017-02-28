7ec47c9b3a3de90de2a3cc8d13c1f577a00ef162
jeff-ma <jefm206@gmail.com>
2017-02-28 13:35:09 -0800

    Added javascript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,14 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
+
+$(document).ready(function(){
+    $(".task-toggle").click(function(){
+    	var SH = this.SH^=1; // "Simple toggler"
+        $(this).siblings(".description").slideToggle();
+        $(this).html(SH?"Hide details":"Show details");
+    });
+});
