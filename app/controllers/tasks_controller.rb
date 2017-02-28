5f1982304810d3ee19debdcd30fd910c3b2bc4a6
jeff-ma <jefm206@gmail.com>
2017-02-28 12:19:06 -0800

    Added views routes controllers

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,58 @@
 class TasksController < ApplicationController
+	before_action :find_task, only: [:show, :edit, :update, :destroy]
+
+	def index
+		if user_signed_in?
+			@tasks = Task.where(:user_id => current_user.id).order("created_at DESC")
+		end
+	end
+
+	def show
+	end
+
+	def new
+		@task = current_user.tasks.build
+	end
+
+	def edit
+	end
+
+	def create
+		@task = current_user.tasks.build(task_params)
+		if @task.save
+			redirect_to root_path
+		else
+			render 'new'
+		end
+	end
+
+	def update
+		if @task.update(task_params)
+			redirect_to root_path
+			#redirect_to task_path(@task)
+		else 
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@task.destroy
+		redirect_to root_path
+	end
+
+	def complete
+		@task = Task.find(params[:id])
+		@task.update_attribute(:completed, Time.now)
+		redirect_to root_path
+	end
+
+	private
+		def task_params
+			params.require(:task).permit(:title, :description, :address)
+		end
+
+		def find_task
+			@task = Task.find(params[:id])
+		end
+
 end
