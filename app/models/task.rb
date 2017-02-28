1a777de65d9e7ce8bb50a24b014b48bf758b24ac
jeff-ma <jefm206@gmail.com>
2017-02-28 21:23:36 +0000

    Change routes and model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,9 @@
 class Task < ApplicationRecord
+  belongs_to :user
+  geocoded_by :address
+  after_validation :geocode
+
+  def completed?
+    !completed.blank?
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
- # devise_for :users
-  #resources :tasks do
-  #  member do
-  #    patch :complete
-  #  end
-  #end
+  devise_for :users
+  resources :tasks do
+    member do
+      patch :complete
+    end
+  end
   root 'tasks#index'
 end
