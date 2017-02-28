e84d906e1c5c62c397be2cc4c0fdbdb53974cbb9
jeff-ma <jefm206@gmail.com>
2017-02-28 21:08:50 +0000

    Finsished install devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :tasks do
-    member do
-      patch :complete
-    end
-  end
+ # devise_for :users
+  #resources :tasks do
+  #  member do
+  #    patch :complete
+  #  end
+  #end
   root 'tasks#index'
 end
